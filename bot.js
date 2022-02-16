a447530bb3255390fcc82821568f6e87430c88e5
wlamason <lamasonian@gmail.com>
2022-02-15 21:51:00 -0500

    remove setActivity on guild change events

--- bot.js
+++ bot.js
@@ -341,12 +341,10 @@ client.on("ready", () => {
 
 client.on("guildCreate", guild => {
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 // Command handler
